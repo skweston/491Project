a6d381bd31e911c89dd9bdf58a2223def0e52c55
Spiritoak <44207781+Spiritoak@users.noreply.github.com>
2019-01-27 16:16:36 -0800

    1/27 4:16
    trying to unfuck this thing

--- main.js
+++ main.js
@@ -1,46 +1,33 @@
-function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop) {
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
     this.spriteSheet = spriteSheet;
-    this.startX = startX;
-    this.startY = startY;
     this.frameWidth = frameWidth;
     this.frameDuration = frameDuration;
     this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
     this.frames = frames;
     this.totalTime = frameDuration * frames;
     this.elapsedTime = 0;
     this.loop = loop;
+    this.scale = scale;
 }
 
-Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
-    var scaleBy = scaleBy || 1;
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
     this.elapsedTime += tick;
-    if (this.loop) {
-        if (this.isDone()) {
-            this.elapsedTime = 0;
-        }
-    } else if (this.isDone()) {
-        return;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
     }
-    var index = this.currentFrame();
-    var vindex = 0;
-    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
-        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
-        vindex++;
-    }
-    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
-        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
-        vindex++;
-    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
 
-    var locX = x;
-    var locY = y;
-    var offset = vindex === 0 ? this.startX : 0;
     ctx.drawImage(this.spriteSheet,
-                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                  this.frameWidth, this.frameHeight,
-                  locX, locY,
-                  this.frameWidth * scaleBy,
-                  this.frameHeight * scaleBy);
+                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                 this.frameWidth, this.frameHeight,
+                 x, y,
+                 this.frameWidth * this.scale,
+                 this.frameHeight * this.scale);
 }
 
 Animation.prototype.currentFrame = function () {
@@ -180,13 +167,15 @@ LaserBlast.prototype.draw = function () {
 /* ========================================================================================================== */
 // The Ship
 /* ========================================================================================================== */
-function TheShip(game) {
-    var width = 128;
-    var height = 128;
-    this.idleAnimation = new Animation(AM.getAsset("./img/theship.png"), 0, 0, width, height, 0.03, 2, true);
-    var x = 400 - (width / 2);
-    var y = 400 - (height / 2);
-    Entity.call(this, game, x, y);
+function TheShip(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 128, 128, 0.03, 2, true, 1);
+    this.speed = 0;
+    this.x = 0;
+    this.y = 0;
+    this.game = game;
+    this.ctx = game.ctx;
+    this.removeFromWorld = false;
+    Entity.call(this, game, this.x, this.y);
 }
 
 TheShip.prototype = new Entity();
@@ -196,8 +185,8 @@ TheShip.prototype.update = function () {
     Entity.prototype.update.call(this);
 }
 
-TheShip.prototype.draw = function (ctx) {
-    this.idleAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+TheShip.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
     Entity.prototype.draw.call(this);
 }
 
@@ -206,7 +195,7 @@ TheShip.prototype.draw = function (ctx) {
 /* ========================================================================================================== */
 var AM = new AssetManager();
 // AM.queueDownload("./img/background.jpg");
-AM.queueDownload("./img/theship.png");
+AM.queueDownload("./img/shipIdle.png");
 AM.queueDownload("./img/Boss1.png");
 AM.queueDownload("./img/BossTurret.png");
 AM.queueDownload("./img/LaserBlast.png");
@@ -217,17 +206,15 @@ AM.downloadAll(function () {
     var ctx = canvas.getContext("2d");
 
     var gameEngine = new GameEngine();
-    var theShip = new TheShip(gameEngine);
+    gameEngine.init(ctx);
+    gameEngine.start();
 
-    gameEngine.addEntity(theShip);
+    gameEngine.addEntity(new TheShip(gameEngine, AM.getAsset("./img/shipIdle.png")));
     gameEngine.addEntity(new Boss1(gameEngine, AM.getAsset("./img/Boss1.png")));
     gameEngine.addEntity(new BossTurret(gameEngine, AM.getAsset("./img/BossTurret.png"), 375, 380));
     gameEngine.addEntity(new BossTurret(gameEngine, AM.getAsset("./img/BossTurret.png"), 310, 520));
     gameEngine.addEntity(new BossTurret(gameEngine, AM.getAsset("./img/BossTurret.png"), 375, 325));
-    gameEngine.addEntity(new BossTurret(gameEngine, AM.getAsset("./img/BossTurret.png"), 435, 520));
-
-    gameEngine.init(ctx);
-    gameEngine.start();
+    gameEngine.addEntity(new BossTurret(gameEngine, AM.getAsset("./img/BossTurret.png"), 435, 520));    
 
     console.log("All Done!");
 });
