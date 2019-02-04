70384e208fde4190179eae0a53817cf28c1617aa
kiltedfrog <44252317+kiltedfrog@users.noreply.github.com>
2019-02-04 00:11:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1288,9 +1288,9 @@ PlayGame.prototype.draw = function (ctx) {
 var AM = new AssetManager();
 
 AM.queueDownload("./img/space1-1.png");
-AM.queueDownload("./img/Uberspace.png");
+// AM.queueDownload("./img/Uberspace.png");
 AM.queueDownload("./img/4kBackground1.png");
-AM.queueDownload("./img/4kBackground2.png");
+// AM.queueDownload("./img/4kBackground2.png");
 // ship stuff
 AM.queueDownload("./img/shipIdle.png");
 AM.queueDownload("./img/shipBoost.png");
