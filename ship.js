9dc199fb1b6f333ea903da75efc1f9dbb570db84
dannycarns <dcarns@uw.edu>
2019-02-07 17:41:08 -0800

    Update ship.js

--- ship.js
+++ ship.js
@@ -3,6 +3,8 @@
 /* ========================================================================================================== */
 
 function TheShip(game) {
+	this.game = game;
+	this.ctx = game.ctx;
 	this.pWidth = 128;
 	this.pHeight = 128;
 	this.scale = 0.5;
@@ -12,16 +14,18 @@ function TheShip(game) {
 	this.boostRollAnimation = new Animation(AM.getAsset("./img/shipBoostRoll.png"), this.pWidth, this.pHeight, 256, 0.03, 22, false, this.scale);
 	this.reticleAnimation = new Animation(AM.getAsset("./img/shipReticle.png"), this.pWidth, this.pHeight, 256, 0.5, 2, true, 0.25);
 
+	this.invincible = false;
 	this.name = "Player";
-    this.invincible = false;
 	this.health = 100;
+	this.boostMax = 1000;
+	this.boost = this.boostMax;
 	this.speed = 0.5;
 	this.boosting = false;
 	this.cancelBoost = false;
 	this.rolling = false;
 	this.rollCooldown = 0;
-	this.x = 400 - (this.pWidth * this.scale / 2);
-	this.y = 400 - (this.pHeight * this.scale / 2);
+	this.x = this.game.cameraCtx.canvas.width/2 - (this.pWidth * this.scale / 2);
+	this.y = this.game.cameraCtx.canvas.height/2 - (this.pHeight * this.scale / 2);
 	this.xMid = (this.x + (this.pWidth * this.scale / 2)) - 1;
 	this.yMid = (this.y + (this.pHeight * this.scale / 2)) - 1;
 	this.radius = this.scale * 64;
@@ -32,9 +36,10 @@ function TheShip(game) {
 	this.secondaryCooldown = 0;
 	this.spreaderLevel = 0;
 	this.spreader = 0;
+	this.boostGainRate = 1;
+	this.boostConsumeRate = 2;
 
-	this.game = game;
-	this.ctx = game.ctx;
+
 	this.removeFromWorld = false;
 	Entity.call(this, game, this.x, this.y);
 }
@@ -48,6 +53,34 @@ TheShip.prototype.update = function () {
 	if(this.health < 1){
 		this.removeFromWorld = true;
 	}
+	// boosting
+	this.speed = 0.5;
+	this.boosting = false;
+
+	if (this.game.boost && !this.rolling && this.boost > 1) {
+		this.cancelBoost = false;
+		this.boosting = true;
+		this.speed = 1;
+		this.boost -= this.boostConsumeRate;
+	}
+	if (!this.game.boost && !this.rolling) {
+		this.boosting = false;
+		this.speed = 0.5;
+		if (this.boost < this.boostMax){
+			this.boost += this.boostGainRate;
+		}
+	}
+
+	// boost input buffer during rolls
+	if (this.game.boost && this.rolling) {
+		this.boosting = true;
+		this.speed = 1;
+		this.cancelBoost = false;
+	}
+	if (!this.game.boost && this.rolling) {
+		this.cancelBoost = true;
+	}
+
 	// movement
 	var xMove = 0;
 	var yMove = 0;
@@ -74,13 +107,23 @@ TheShip.prototype.update = function () {
 
 	if (xMove === 0) {
 		this.y += yMove;
+		if(this.game.camera.isScrolling){
+			this.game.mouseY += yMove;
+		}
 	}
 	else if (yMove === 0) {
 		this.x += xMove;
+		if(this.game.camera.isScrolling){
+			this.game.mouseX += xMove;
+		}
 	}
 	else {
 		this.x += xMove * 0.7;
 		this.y += yMove * 0.7;
+		if(this.game.camera.isScrolling){
+			this.game.mouseX += xMove * 0.7;
+	 		this.game.mouseY += yMove * 0.7;
+		}
 	}
 
 	// update center hitbox
@@ -96,7 +139,6 @@ TheShip.prototype.update = function () {
 	if (this.game.roll && this.rollCooldown === 0) {
 		this.rollCooldown = 100;
 		this.rolling = true;
-        this.invincible = true;
 	}
 	if (this.rollCooldown > 0) {
 		this.rollCooldown -= 1;
@@ -105,7 +147,6 @@ TheShip.prototype.update = function () {
 		if (this.rollAnimation.isDone()) {
 			this.rollAnimation.elapsedTime = 0;
 			this.rolling = false;
-            this.invincible = false;
 		}
 		else if (this.boostRollAnimation.isDone()) {
 			this.boostRollAnimation.elapsedTime = 0;
@@ -117,24 +158,7 @@ TheShip.prototype.update = function () {
 		}
 	}
 
-	// boosting
-	if (this.game.boost && !this.rolling) {
-		this.cancelBoost = false;
-		this.boosting = true;
-		this.speed = 1;
-	}
-	if (!this.game.boost && !this.rolling) {
-		this.boosting = false;
-		this.speed = 0.5;
-	}
 
-	// boost input buffer during rolls
-	if (this.game.boost && this.rolling) {
-		this.cancelBoost = false;
-	}
-	if (!this.game.boost && this.rolling) {
-		this.cancelBoost = true;
-	}
 
 	// shooting
 	if (this.primaryCooldown > 0) {
